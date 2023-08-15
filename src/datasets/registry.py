1432e4fbcd19b6e2b8bb23ead7408d3c06787d86
Reza Davari <rezazzr@hotmail.com>
2023-08-15 18:44:40 -0400

    lowered the default number of workers for data loading

--- src/datasets/registry.py
+++ src/datasets/registry.py
@@ -74,7 +74,7 @@ def split_train_into_train_val(
 
 
 def get_dataset(
-    dataset_name, preprocess, location, batch_size=128, num_workers=16, val_fraction=0.1, max_val_samples=5000
+    dataset_name, preprocess, location, batch_size=128, num_workers=6, val_fraction=0.1, max_val_samples=5000
 ):
     if dataset_name.endswith("Val"):
         # Handle val splits
